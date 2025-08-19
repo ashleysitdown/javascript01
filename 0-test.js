4880f995cb5a88f104867f6d158b196370df9839
ashleysitdown <evanashley.ng@gmail.com>
2025-08-19 13:33:43 +0000

    gitpushtry

---
+++ 0-test.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+
+//console.log((2+5)/(2+3))
+//console.log((7)/(5))
+
+//console.log( 2 * 3 + 10 / 2 ** 4)
+//console.log( 6 + 10 / 16)
+//console.log( 6 + 0.625)
+
+//console.log( 4 % 2 * 3)
+//console.log( 0 * 3)
+
+
